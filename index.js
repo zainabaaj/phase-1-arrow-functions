fc835a0eb1882ec437329651ad57d197b2eaad74
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-05 18:09:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,3 @@
+const divide = function (parameter1, parameter2) { return parameter1 / parameter2 }
+const square = value => value * value
+const add = (parameter1, parameter2) => parameter1 + parameter2
